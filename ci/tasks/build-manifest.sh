7d85216a134ae458d82072669b768fdca32b27cd
boparp-pivotal <pbopardikar@pivotal.io>
2016-04-19 08:30:59 -0700

    dummy change

--- ci/tasks/build-manifest.sh
+++ ci/tasks/build-manifest.sh
@@ -5,15 +5,10 @@ set -e
 source app-src/ci/tasks/utils.sh
 
 check_param application_name
-check_param application_domain
-check_param application_hostname
 check_param num_instances
-check_param cloud
-check_param dashboard_url
-check_param redis_service_name
 
 manifests_dir="${PWD}/manifests"
-manifest_file="${manifests_dir}/manifest-${cloud}.yml"
+manifest_file="${manifests_dir}/manifest.yml"
 
 application_version=`cat app-version/number`
 
@@ -22,15 +17,12 @@ cat > ${manifest_file} <<EOF
 ---
 applications:
  - name: ${application_name}
-   domain: ${application_domain}
-   host: ${application_hostname}
+   host: ${application_name}
    instances: ${num_instances}
-   memory: 256M
-   disk_quota: 256M
+   memory: 512M
+   disk_quota: 512M
+   path: {PWD}/build/libs/cities-service.jar
+
    env:
-     cloud: ${cloud}
-     dashboard: ${dashboard_url}
-     version: ${application_version}
-   services:
-     - ${redis_service_name}
+     SPRING_PROFILES_ACTIVE: cloud
 EOF
